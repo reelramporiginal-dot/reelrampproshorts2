6b9a45989ea052b371c8a1465046b0509b587b31
reelramporiginal-dot <reelramporiginal@gmail.com>
2026-05-20 23:49:57 -0700

    Delete api directory

--- api/_wake.js
+++
@@ -1,7 +0,0 @@
-export default async function handler(req, res) {
-  res.setHeader('Access-Control-Allow-Origin', '*');
-  res.setHeader('Access-Control-Allow-Methods', 'GET, OPTIONS');
-  res.setHeader('Access-Control-Allow-Headers', 'Content-Type, Authorization');
-  if (req.method === 'OPTIONS') return res.status(204).end();
-  return res.status(200).json({ ok: true, service: 'ReelRamp Pro API awake' });
-}
